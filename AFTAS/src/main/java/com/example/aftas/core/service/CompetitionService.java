30e78b9e60978c62be0c9a6e77ef4307e34c8db9
mohamed chkir <mohamedchkir2@gmail.com>
2023-12-21 14:42:45 +0100

    edite in getAllCompetitionsWithStatus Function to return a page and add getCompetitionByCode methode in CompetitionService interface

--- AFTAS/src/main/java/com/example/aftas/core/service/CompetitionService.java
+++ AFTAS/src/main/java/com/example/aftas/core/service/CompetitionService.java
@@ -4,12 +4,14 @@ import com.example.aftas.core.dao.model.dto.Get.GetCompetitionDto;
 import com.example.aftas.core.dao.model.dto.Get.GetRankingDto;
 import com.example.aftas.core.dao.model.dto.Store.CompetitionDto;
 import com.example.aftas.core.dao.model.entity.Competition;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CompetitionService {
     CompetitionDto addCompetition(CompetitionDto competitionDto);
-    List<GetCompetitionDto> getAllCompetitionsWithStatus();
+    Page<GetCompetitionDto> getAllCompetitionsWithStatus(int page, int size);
     List<GetRankingDto> calculateScore(String competitionCode);
-
+    GetCompetitionDto getCompetitionByCode(String competitionCode);
 }
