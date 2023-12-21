79f44b9116a24d772132544dc9404f2125757372
mohamed chkir <mohamedchkir2@gmail.com>
2023-12-21 14:37:11 +0100

    edite in CompetitionCodeGenerator make the tree first alphabet UpperCase

--- AFTAS/src/main/java/com/example/aftas/common/helper/Validation/Compitition/CompetitionCodeGenerator.java
+++ AFTAS/src/main/java/com/example/aftas/common/helper/Validation/Compitition/CompetitionCodeGenerator.java
@@ -5,7 +5,7 @@ import com.example.aftas.core.dao.model.dto.Store.CompetitionDto;
 public class CompetitionCodeGenerator {
     public static String generateCode(CompetitionDto competitionDto) {
         String location = competitionDto.getLocation();
-        String locationDigits = location.substring(0, Math.min(location.length(), 3));
+        String locationDigits = location.substring(0, Math.min(location.length(), 3)).toUpperCase();
         return locationDigits + "-" + competitionDto.getDate().toString();
     }
 }
