ee096e518f968769d19a8d7b1128973a1a914d77
mohamed chkir <mohamedchkir2@gmail.com>
2024-02-29 14:54:16 +0100

    SECURITY: add needed entities for security (role and permissions)

--- BCM/src/main/java/org/example/bcm/core/model/entity/Permission.java
+++ BCM/src/main/java/org/example/bcm/core/model/entity/Permission.java
@@ -1,5 +1,29 @@
 package org.example.bcm.core.model.entity;
-//todo: implement entity
 
-public class Permission {
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Permission implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @ManyToMany(mappedBy = "permissions")
+    private List<Role> roles;
+
+    @Override
+    public String getAuthority() {
+        return name;
+    }
 }
