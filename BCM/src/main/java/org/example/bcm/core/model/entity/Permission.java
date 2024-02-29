d6cd2750bdec2b9d01ab98d8db5ad3bbfd62af51
mohamed chkir <mohamedchkir2@gmail.com>
2024-02-29 14:20:00 +0100

    DEV: Add entities without implement them

---
+++ BCM/src/main/java/org/example/bcm/core/model/entity/Permission.java
@@ -0,0 +1,5 @@
+package org.example.bcm.core.model.entity;
+//todo: implement entity
+
+public class Permission {
+}
---
+++ BCM/src/main/java/org/example/bcm/core/model/entity/Token.java
@@ -0,0 +1,4 @@
+package org.example.bcm.core.model.entity;
+//todo: implement entity
+public class Token {
+}
